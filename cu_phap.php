2101b8e66bf65300edcd1ba761e8d275bce22f56
thao <capthao2013@gmail.com>
2017-09-30 02:14:18 -0700

    php cu_phap

---
+++ cu_phap.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>cu phap php</title>
+  </head>
+  <body>
+    <?php
+      echo "vi du minh hoa ve echo <br>";
+      print "vi du minh hoa lenh print";
+    ?>
+  </body>
+</html>
