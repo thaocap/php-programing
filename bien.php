2aaa592b111c6c6b1e3a7674c93a7c532d4a313b
thao <capthao2013@gmail.com>
2017-10-14 01:45:53 -0700

    bienphp

---
+++ bien.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>bien trong php</title>
+</head>
+<body>
+<?php
+    define("PI", 3.14);
+    define("HOTGIRL", 'Ngan tit');
+
+    $name = "Cap Thi Thao";
+    $address = "quang tri";
+    $old =20;
+    echo "ten: $name  <br>";
+    echo"*****************";
+    echo"dia chi :$address <br>";
+    echo"*******************";
+    echo "tuoi: $old <br>";
+    echo"*******************";
+    echo HOTGIRL;
+?>
+</body>
+</html>
