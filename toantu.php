7e45fa9775fa9f14417fa70c65c888638635e39e
thao <capthao2013@gmail.com>
2017-10-14 02:44:33 -0700

    toan_tu

---
+++ toantu.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>vi du ve toan tu</title>
+</head>
+<body>
+<?php
+   $a = 5;
+   $b = 6 ;
+   $tong = $a + $b;
+   echo "Tong cua $a + $b = $tong";
+?>
+</body>
+</html>
